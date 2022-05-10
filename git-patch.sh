459a051ccfc3903e9f44edea3337a2070fc9ffda
Michael Steinmötzger <m.steinmoetzger@gmail.com>
2022-05-10 14:25:31 +0200

    patch from 10.05.2022 14:25:31 pushed by shortping
     added clean file - added changes to patch file

--- git-patch.sh
+++ git-patch.sh
@@ -1,5 +1,12 @@
 date=$(date '+%d.%m.%Y %H:%M:%S')
 
+commitChanges=""
+
+for changes in "$@"
+do
+  commitChanges="$commitChanges $changes"
+done
+
 git add --all
-git commit -m "patch from $date pushed by $(whoami)"
+git commit -m "patch from $date pushed by $(whoami)" -m "$commitChanges"
 git push -u origin master
